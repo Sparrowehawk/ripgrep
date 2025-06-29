3dc2686ba5a055a9deb27568429acaab81c2c339
Hamid Jamal <hamidlikestocode101@gmail.com>
2025-06-29 15:33:27 +0100

    Added recursive searching

--- ripgrep/src/main.rs
+++ ripgrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::{self};
 use anyhow::{Result, Context};
 use regex::Regex;
-
+use walkdir::WalkDir;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -17,17 +17,29 @@ fn main() -> Result<()> {
     println!("Pattern is: {}", args.pattern.to_lowercase());
     println!("Path is: {:?}\n", args.path);
 
+    let ignores = [".exe", ".psd", ".mp4", ".gitignore", ".tmp"];
+
     let re = Regex::new(&args.pattern)
         .with_context(|| "Could not compile regex")?;
 
-    let contents = fs::read_to_string(&args.path)
-                      .with_context(|| format!("Failed to read from {}", args.path.display()))?;
 
-    for (i, line) in contents.lines().enumerate() {
-        if re.is_match(line){
-            println!("{}. {line}", i+1);
-        }
-    }
+    for entry in WalkDir::new(args.path)
+                .into_iter()
+                .filter_map(Result::ok)
+                .filter(|e| {
+                    !ignores.iter().any(|i| e.path().ends_with(i))
+                })
+                .filter(|e| e.file_type().is_file()){
+                    let path = fs::read_to_string(entry.path())
+                                   .with_context(|| format!("Failed to read from {}", entry.path().display()))?; 
+
+                    for (i, line) in path.lines().enumerate() {
+                        if re.is_match(line){
+                            println!("Found in : {}", entry.path().display());
+                            println!("{}. {line}", i+1);
+                        }
+                    }
+                };
 
     Ok(())
 }
