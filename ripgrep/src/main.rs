e2329132eb969a17686555f2f113d3301450dfbd
Hamid Jamal <67792804+Sparrowehawk@users.noreply.github.com>
2025-06-29 16:44:59 +0100

    Commentated main.rs

--- ripgrep/src/main.rs
+++ ripgrep/src/main.rs
@@ -23,11 +23,14 @@ struct Cli {
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-    println!("Pattern is: {}", args.pattern.to_lowercase());
-    println!("Path is: {:?}\n", args.path);
+    // Troubleshooting commands
+    // println!("Pattern is: {}", args.pattern.to_lowercase());
+    // println!("Path is: {:?}\n", args.path);
 
+    // So that we don't read anything bad
     let ignores = [".exe", ".psd", ".mp4", ".gitignore", ".tmp"];
 
+    // Creates a regex of the pattern and a multi-thread builder
     let re = regex::RegexBuilder::new(&args.pattern)
         .case_insensitive(args.ignore_case)
         .build()
@@ -36,8 +39,11 @@ fn main() -> Result<()> {
     let walk_buider = WalkBuilder::new(&args.path);
 
     walk_buider.build_parallel().run(|| {
+                // Local vars, tbh they are redundent but make readability better
                 let local_re = &re;
                 let local_args = &args;
+
+                // Per thread for each line in each file
 
                 Box::new(move |result| {
                     let entry = match result {
@@ -47,24 +53,30 @@ fn main() -> Result<()> {
                             return WalkState::Continue;
                         }
                     };
-
+                    
+                    // Removes files from earlier include
                     if ignores.iter().any(|i| entry.path().ends_with(i)){
                         return WalkState::Continue
                     }
 
+                    // Makes sure its a file not a dir
                     if !entry.file_type().is_some_and(|ft| ft.is_file()){
                         return  WalkState::Continue;
                     }
 
 
                     if let Ok(contents) = fs::read_to_string(entry.path()){
+                        // For every line, create an int with it and search per
                         for (i, line) in contents.lines().enumerate(){
+                            // Cross refrence with the is_match from the struct to determine what to do
                             let is_match = local_re.is_match(line);
 
+                            // Xor means that it chooses one, not both. Good error handling imo
                             if is_match ^ local_args.invert_match {
                                 let out = if local_args.invert_match {
                                     format!("{} : {} : {}", entry.path().display().to_string().green(), i+1, line)
                                 } else {
+                                    // Split the result into pre and post indexed words
                                     let mat = local_re.find(line).unwrap();
                                     format!("{} : {} : {} {} {}", 
                                             entry.path().display().to_string().green(),
@@ -82,11 +94,6 @@ fn main() -> Result<()> {
 
                     WalkState::Continue
                 })
-                    
-                    
-                    
-            
-            
             });
 
     Ok(())
