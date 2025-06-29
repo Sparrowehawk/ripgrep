ec8c8aafa0fa34d404ff8b08dcb1e4870005e7f8
Hamid Jamal <hamidlikestocode101@gmail.com>
2025-06-29 12:37:18 +0100

    Cli interface added

--- ripgrep/src/main.rs
+++ ripgrep/src/main.rs
@@ -1,8 +1,22 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn main() -> std::io::Result<()>{
-    let mut file = File::open("./poem.txt")?;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    pattern: String, 
+    path: std::path::PathBuf,
+}
+
+
+fn main() -> std::io::Result<()> {
+    let args = Cli::parse();
+    println!("Pattern is: {}", args.pattern);
+    println!("Path is: {:?}", args.path);
+
+    let mut file = File::open(args.path)?;
     let mut contents = String::new();
 
     file.read_to_string(&mut contents)?;
---
+++ ripgrep/target/debug/deps/anstyle_parse-5631284ba74a3127.d
@@ -0,0 +1,9 @@
+/home/sparrowe/Documents/rust_projects/ripgrep/ripgrep/target/debug/deps/anstyle_parse-5631284ba74a3127.d: /home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/lib.rs /home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/params.rs /home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/mod.rs /home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/definitions.rs /home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/table.rs
+
+/home/sparrowe/Documents/rust_projects/ripgrep/ripgrep/target/debug/deps/libanstyle_parse-5631284ba74a3127.rmeta: /home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/lib.rs /home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/params.rs /home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/mod.rs /home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/definitions.rs /home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/table.rs
+
+/home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/lib.rs:
+/home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/params.rs:
+/home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/mod.rs:
+/home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/definitions.rs:
+/home/sparrowe/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/table.rs:
