cdb83c58ae39c91a56ad0801a9fdd48798cc9974
Hamid Jamal <hamidlikestocode101@gmail.com>
2025-06-29 16:05:32 +0100

    flags

--- ripgrep/src/main.rs
+++ ripgrep/src/main.rs
@@ -1,14 +1,23 @@
 use std::fs::{self};
 use anyhow::{Result, Context};
-use regex::Regex;
-use walkdir::WalkDir;
+use ignore::Walk;
 use clap::Parser;
+use colored::Colorize;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
     pattern: String, 
     path: std::path::PathBuf,
+
+    // Flag time
+    /// Make the search case-insensitive
+    #[arg(short = 'i', long)]
+    ignore_case: bool,
+
+    /// Invert the search to find lines that DON'T match
+    #[arg(short = 'v', long)]
+    invert_match: bool,
 }
 
 
@@ -19,24 +28,37 @@ fn main() -> Result<()> {
 
     let ignores = [".exe", ".psd", ".mp4", ".gitignore", ".tmp"];
 
-    let re = Regex::new(&args.pattern)
+    let re = regex::RegexBuilder::new(&args.pattern)
+        .case_insensitive(args.ignore_case)
+        .build()
         .with_context(|| "Could not compile regex")?;
 
 
-    for entry in WalkDir::new(args.path)
-                .into_iter()
+    for entry in Walk::new(args.path)
                 .filter_map(Result::ok)
                 .filter(|e| {
                     !ignores.iter().any(|i| e.path().ends_with(i))
                 })
-                .filter(|e| e.file_type().is_file()){
+                .filter(|e| e.file_type().expect("REASON").is_file()){
                     let path = fs::read_to_string(entry.path())
                                    .with_context(|| format!("Failed to read from {}", entry.path().display()))?; 
 
                     for (i, line) in path.lines().enumerate() {
-                        if re.is_match(line){
-                            println!("Found in : {}", entry.path().display());
-                            println!("{}. {line}", i+1);
+                        let is_match = re.is_match(line);
+
+                        if is_match ^ args.invert_match {
+                            if args.invert_match {
+                                println!("{}: {}: {}", entry.path().display().to_string().green(), i + 1, line);
+                            } else {
+                                let mat = re.find(line).unwrap();
+                                let start = mat.start();
+                                let end = mat.end();
+
+                                
+                                print!("{}: {}: {}", entry.path().display().to_string().green(), i + 1, &line[..start]);
+                                print!("{}", &line[start..end].blue().bold());
+                                println!("{}", &line[end..]);
+                            }
                         }
                     }
                 };
