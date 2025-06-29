439d8c8a381418dcd8e4c690f5d05c6310e72632
Hamid Jamal <hamidlikestocode101@gmail.com>
2025-06-29 14:44:46 +0100

    regex support

--- ripgrep/src/main.rs
+++ ripgrep/src/main.rs
@@ -1,4 +1,6 @@
 use std::fs::{self};
+use anyhow::{Result, Context};
+use regex::Regex;
 
 use clap::Parser;
 
@@ -10,16 +12,20 @@ struct Cli {
 }
 
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<()> {
     let args = Cli::parse();
-    println!("Pattern is: {}", args.pattern);
+    println!("Pattern is: {}", args.pattern.to_lowercase());
     println!("Path is: {:?}\n", args.path);
 
-    let contents = fs::read_to_string(&args.path)?;
+    let re = Regex::new(&args.pattern)
+        .with_context(|| "Could not compile regex")?;
 
-    for line in contents.lines() {
-        if line.contains(&args.pattern) {
-            println!("{line}");
+    let contents = fs::read_to_string(&args.path)
+                      .with_context(|| format!("Failed to read from {}", args.path.display()))?;
+
+    for (i, line) in contents.lines().enumerate() {
+        if re.is_match(line){
+            println!("{}. {line}", i+1);
         }
     }
 
